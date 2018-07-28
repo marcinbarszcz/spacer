e87b40739d648e97989dcc693ccd1ca6dee5e040
marcinbarszcz <marbar4466@gmail.com>
2018-07-28 22:57:11 +0200

    Finish]

---
+++ src/components/Claim.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="wrapper">
+        <h1 class="claim">SPACER</h1>
+        <p class="subclaim">
+            Begin your journey through our amazing galaxy, and discover places you never ever heard of.
+        </p>
+        <p class="subclaim">
+            Type anything space-related to start.
+        </p>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Claim',
+};
+</script>
+<style lang="scss" scoped>
+    .wrapper {
+        display: flex;
+        text-align: center;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: white;
+    }
+
+    .claim {
+        font-size: 50px;
+        margin: 0;
+
+        @media (min-width: 768px) {
+            font-size: 90px;
+        }
+
+        @media (min-width: 1024) {
+            font-size: 110px;
+        }
+    }
+
+    .subclaim {
+        margin: 20px 0 0 0;
+        font-size: 13px;
+        font-weight: 300;
+
+        @media (min-width: 768px) {
+            font-size: 17px;
+            line-height: 1;
+            letter-spacing: 1px;
+            width: 500px;
+        }
+    }
+</style>
---
+++ src/components/SearchInput.vue
@@ -0,0 +1,69 @@
+<template>
+<div class="wrapper">
+    <input
+    id="search"
+    name="search"
+    :class="{ dark }"
+    :value="value"
+    @input="handleChange"
+    />
+</div>
+</template>
+<script>
+export default {
+  name: 'SearchInput',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleChange(e) {
+      this.$emit('input', e.target.value);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+}
+input {
+  color: white;
+  text-align: center;
+  font-size: 18px;
+  font-weight: 300;
+  height: 30px;
+  border: 0;
+  background: none;
+  border-bottom: 1px solid white;
+  transition: box-shadow 0.3s ease-out;
+
+  @media (min-width: 1024px) {
+    font-weight: 400;
+    font-size: 25px;
+    letter-spacing: 2px;
+  }
+}
+
+input:focus {
+  outline: none;
+  box-shadow: 0 10px 10px -8px rgba(255, 255, 255, 0.5);
+}
+
+.dark {
+  color: #1e3d4a;
+  border-bottom-color: #1e3d4a;
+}
+.dark:focus {
+  box-shadow: 0 10px 10px -8px rgba(#1e3d4a, 0.2);
+}
+</style>
