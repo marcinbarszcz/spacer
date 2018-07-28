10ce520835cddf4f6caa7a53196c15781b3aa622
marcinbarszcz <marbar4466@gmail.com>
2018-07-28 19:08:31 +0200

    Add Nasa Api

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Search from './views/Search.vue';
+import About from './views/About.vue';
 
 Vue.use(Router);
 
@@ -9,11 +10,11 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: Search,
     },
     {
       path: '/about',
-      name: 'About',
+      name: 'about',
       component: About,
     },
   ],
